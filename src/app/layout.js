a68c6eddb8aaa7a5d66e1d422eb6fa960730feb1
Rghv77 <harshraghav718@gmail.com>
2024-08-29 15:55:38 +0530

    output removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+"use client";
+
 import "./globals.scss";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
@@ -6,19 +8,21 @@ import "slick-carousel/slick/slick-theme.css";
 import Header from "@/components/Nav";
 import Head from "next/head";
 import Gatag from "./Gatag.js";
+import { Outlet } from 'react-router-dom';
+// import AppRouter from './AppRouter';
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "HeyBuddy",
-  icons: {
-    icon: '/favicon.ico',
-  },
+// export const metadata = {
+//   title: "HeyBuddy",
+//   icons: {
+//     icon: '/favicon.ico',
+//   },
 
-  description: "HeyBuddy Website",
-};
+//   description: "HeyBuddy Website",
+// };
 
-export default function RootLayout({ children }) {
+export default function RootLayout() {
   return (
     <html lang="en">
        <Head>
@@ -27,7 +31,9 @@ export default function RootLayout({ children }) {
       <Gatag />
       <body className={inter.className}>
         <Header />
-        <div className=" mx-auto min-h-[100vh]">{children}</div>
+        <div className=" mx-auto min-h-[100vh]">
+            <Outlet />
+          </div>
         <Footer />
       </body>
     </html>

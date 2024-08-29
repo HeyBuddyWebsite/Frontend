32f4bb8bc3875f380226410b035c1d16fee10c0e
Rghv77 <harshraghav718@gmail.com>
2024-08-29 16:22:30 +0530

    configuration changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-"use client";
+
 
 import "./globals.scss";
 import { Inter } from "next/font/google";
@@ -8,21 +8,20 @@ import "slick-carousel/slick/slick-theme.css";
 import Header from "@/components/Nav";
 import Head from "next/head";
 import Gatag from "./Gatag.js";
-import { Outlet } from 'react-router-dom';
-// import AppRouter from './AppRouter';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: "HeyBuddy",
-//   icons: {
-//     icon: '/favicon.ico',
-//   },
+export const metadata = {
+  title: "HeyBuddy",
+  icons: {
+    icon: '/favicon.ico',
+  },
 
-//   description: "HeyBuddy Website",
-// };
+  description: "HeyBuddy Website",
+};
 
-export default function RootLayout() {
+export default function RootLayout({children}) {
   return (
     <html lang="en">
        <Head>
@@ -32,7 +31,7 @@ export default function RootLayout() {
       <body className={inter.className}>
         <Header />
         <div className=" mx-auto min-h-[100vh]">
-            <Outlet />
+            {children}
           </div>
         <Footer />
       </body>

ff87313f5bb25a2f340c6187463e1d19c3aa9041
vipulHeybuddy <149054269+vipulHeybuddy@users.noreply.github.com>
2025-11-21 18:28:47 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,22 +4,49 @@ import Footer from "@/components/Footer";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Header from "@/components/Nav";
-import Head from "next/head";
+import Script from "next/script";
 import Gatag from "./Gatag.js";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "HeyBuddy",
-
   description: "HeyBuddy Website",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Gatag />
+      <head>
+        <Gatag />
+      </head>
       <body className={inter.className}>
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '792540776887763');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=792540776887763&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
@@ -30,6 +57,7 @@ export default function RootLayout({ children }) {
           ></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+        
         <Header />
         {children}
         <Footer />

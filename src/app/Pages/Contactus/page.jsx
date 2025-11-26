32a8bd3561378d63369051d869cd3481b708dec0
vipulHeybuddy <149054269+vipulHeybuddy@users.noreply.github.com>
2025-11-26 14:24:23 +0530

    Update page.jsx

--- src/app/Pages/Contactus/page.jsx
+++ src/app/Pages/Contactus/page.jsx
@@ -17,7 +17,7 @@ const Page = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const apiEndpoint = "https://api.heybuddy.co.in/contact";
+    const apiEndpoint = "https://emails-backend-ghaeffhnfmcbfscv.eastus-01.azurewebsites.net/contact";
 
     try {
       setIsSubmitting(true);
